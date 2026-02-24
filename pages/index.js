4f4f9af9d82ae64c129eff8f2c823ab1fb35d246
Tiago de Andrade Lima <tiago.andrade.lima@gmail.com>
2026-02-24 00:06:15 -0300

    restauracao

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,5 @@
 function Home() {
   return <h1>Vida, Te amo❤️</h1>;
 }
-function pic() {
-  return (
-    <img
-      src="https://photos.google.com/u/1/photo/AF1QipOD5bIixPkrsgUEHmhiWNYHWgHQHrMbE9_XUcE7"
-      alt="Vida"
-    />
-  );
-}
+
 export default Home;
